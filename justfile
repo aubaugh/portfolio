44b8b856b32178aab0d17c077218f6a03f436c6b
Austin Baugh <abaugh@pdx.edu>
2019-11-18 11:03:42 -0800

    Updated config file structure and added content

--- justfile
+++ justfile
@@ -3,10 +3,11 @@
 # End:
 # vim: set ft=make :
 
-output-dir := "../austinsheep.github.io"
+output-dir := "austinsheep.github.io"
 
 # Build portfolio
 build:
     mkdir -p {{output-dir}}
-    OUTPUT_CSS={{output-dir}}+"/app.css" wasm-pack build --target web --no-typescript --out-dir {{output-dir}}
+    OUTPUT_CSS={{output-dir}}/app.css wasm-pack build --target web --no-typescript --out-dir {{output-dir}}
+    rm {{output-dir}}/.gitignore
     cp index.html {{output-dir}}
--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,29 @@ use web_sys::MouseEvent;
 
 use css_rs_macro::css;
 use virtual_dom_rs::prelude::*;
+use std::collections::HashMap;
 
 use ron::de::from_str;
 use serde::Deserialize;
 
 /// The configuration structure that is included and deserialized from `config.ron`
 #[derive(Debug, Deserialize)]
-struct ProjectConfig {
+struct Portfolio {
     name: String,
-    description: String,
-    url: String,
+    email: String,
+    about: String,
+    languages: HashMap<String, String>,
+    technologies: HashMap<String, String>,
+    projects: Vec<Project>
 }
 #[derive(Debug, Deserialize)]
-struct PortfolioConfig {
+struct Project {
     name: String,
-    about: String,
-    projects: Vec<ProjectConfig>
+    role: String,
+    languages: Vec<String>,
+    technologies: Vec<String>,
+    description: String,
+    url: String,
 }
 
 #[wasm_bindgen]
@@ -34,20 +41,54 @@ impl App {
     #[wasm_bindgen(constructor)]
     pub fn new () -> App {
         let config_file = include_str!("../config.ron");
-        let config: PortfolioConfig = from_str(config_file).unwrap();
-        let projects: Vec<VirtualNode> = config.projects.iter().map(|project| {
+        let config: Portfolio = from_str(config_file).unwrap();
+        let projects: Vec<VirtualNode> = config.projects
+            .iter().map(|project| {
+            let languages: Vec<VirtualNode> = project.languages
+                .iter().map(|language| {
+                html! {
+                    <a href=config.languages[&language.to_string()].to_string()>
+                        { language.to_string() }
+                    </a>
+                }
+            }).collect();
+            let technologies: Vec<VirtualNode> = project.technologies
+                .iter().map(|technology| {
+                html! {
+                    <a href=config.technologies[&technology.to_string()].to_string()>
+                        { technology.to_string() }
+                    </a>
+                }
+            }).collect();
             html! {
                 <div>
-                    <a href=project.url.to_string()>{ project.name.to_string() }</a>
+                    <p>
+                        <a href=project.url.to_string() class="inline">
+                            { project.name.to_string() }
+                        </a>
+                        <span class="right">
+                            <b>Role:</b> { project.role.to_string() }
+                        </span>
+                    </p>
                     <p>{ project.description.to_string() }</p>
+                    <p>
+                        Languages: <em> { languages } </em>
+                        <span class="right">
+                            Technologies: <em> { technologies } </em>
+                        </span>
+                    </p>
                 </div>
             }
         }).collect();
 
+        let mut email_url = String::from("mailto:");
+        email_url.push_str(&config.email);
+
         let view = html! {
             <div>
                 <h2>{ config.name }</h2>
-                <h3>About: { config.about }</h3>
+                <em><a href=email_url>{ config.email }</a></em>
+                <p><b>About: </b>{ config.about }</p>
                 { projects }
             </div>
         };
@@ -63,7 +104,7 @@ impl App {
 }
 
 static _STYLE: &'static str = css!{r#"
-    h2, h3 {
+    h2 {
         text-align: center;
     }
 
@@ -73,5 +114,13 @@ static _STYLE: &'static str = css!{r#"
         padding: 25px;
         margin: 5px auto;
         max-width: 75%;
+    }
+
+    a {
+        margin-right: 5px;
+    }
+
+    .right {
+        float: right;
     }
 "#};
